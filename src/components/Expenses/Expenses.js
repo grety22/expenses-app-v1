973a163ecc18781af5111db05b60d1b5c6c14221
Gretel Martinez <23240307+grety22@users.noreply.github.com>
2021-11-25 18:29:07 -0500

    expenses-app-v1

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+
+export default function Expenses(props) {
+    const items = props.items;
+    const allExpenses = items.map((item) =>
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+    );
+    return <Card className="expenses">{allExpenses}</Card>;
+}
